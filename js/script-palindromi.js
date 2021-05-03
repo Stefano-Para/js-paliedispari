07bcf3e5b6aaa218fbccdfdceb08bfc2217bbb5e
Stefano-Para <stefano.zina@gmail.com>
2021-05-03 17:53:17 +0200

    prova

--- js/script-palindromi.js
+++ js/script-palindromi.js
@@ -1,23 +1,34 @@
 // 1 - Palindroma
 // Chiedere all'utente di inserire una parola
 var word = prompt("Inserisci qui una parola a tua scelta:");
-console.log(word)
-var reversedWord;
+console.log("parola scelta è " + word)
+
+var reversedWord="";
+
+// prima non c'era il ""
+
 
 
 // giri la parola
 
-for(i = word.length-1; i >= 0; i--) {
+for (i = word.length-1; i >= 0; i--) {
     // riempire la variabile reversedWord 
-    console.log(word[i]);
-
-    
-    
-    
-
+    console.log("la parola è composta da " + word[i]);
+    reversedWord += word[i];
+    console.log(reversedWord)
     // reverseWord.push(word[i]) <--- NO!! solo con array
     // reverseWord += lettera...
 }
+
+if (word == reversedWord) {
+    alert("palindromo")
+} else {
+    alert("non è palindromo")
+}
+    
+    
+
+
 
 
 
