644c2907de92e9ae16abe1f8bb86ece1bf62236e
Stefano-Para <stefano.zina@gmail.com>
2021-05-03 15:55:47 +0200

    errore

---
+++ js/script-palindromi.js
@@ -0,0 +1,29 @@
+// 1 - Palindroma
+// Chiedere all'utente di inserire una parola
+var input = prompt("Inserisci una parola a tuo scelta: Scopri se è un palindromo!");
+console.log("questa è la parola inserita: " + input);
+
+// Creare una funzione per capire se la parola inserita è palindroma
+
+var array = [];
+
+
+var arrayFilled = array.push(input);
+
+console.log("Queste è il quantitativo di item contenuto in ARRAY dopo il push " + arrayFilled);
+
+for (var i=0; i<array.length; i++) {
+    console.log(array)
+    var minuscolo = array.toLowerCase();
+
+}
+
+
+
+
+
+
+
+
+
+
